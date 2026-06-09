28664638fd36fe492357dba01154684df4029d5c
Nik Imran <124644184+yuza101@users.noreply.github.com>
2026-06-10 00:02:39 +0800

    Change welcome message in main.py

--- source-deploy-app/main.py
+++ source-deploy-app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "<h1>Welcome to Deploy-Source-App</h1>"
+    return "<h1>Welcome to my world!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
